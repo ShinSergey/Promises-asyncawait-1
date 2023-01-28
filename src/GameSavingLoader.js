5ec6cd0561e46aca6190bd6933b33d0bed1d9bf1
Shin Sergey <shin.sergey.t@gmail.com>
2023-01-28 19:39:10 +0600

    28.01

--- src/GameSavingLoader.js
+++ src/GameSavingLoader.js
@@ -5,9 +5,9 @@ import GameSaving from './GameSaving';
 export default class GameSavingLoader {
   static load() {
     return read()
-      .then((responce1) => json(responce1))
-      .then((responce2) => {
-        const obj = JSON.parse(responce2);
+      .then((responce) => json(responce))
+      .then((jsoned) => {
+        const obj = JSON.parse(jsoned);
         return new GameSaving(obj.id, obj.created, obj.userInfo);
       });
   }
